82a008499b3c9554eb04466f25f3f36592c8ed7b
qustacmersxf <1725538564@qq.com>
2017-04-07 16:48:14 +0800

    增加后台自动更新天气功能

--- app/src/main/java/com/example/elephantfly/coolweather/WeatherActivity.java
+++ app/src/main/java/com/example/elephantfly/coolweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.example.elephantfly.coolweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -203,6 +204,9 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 
     private void loadBingPic(){
